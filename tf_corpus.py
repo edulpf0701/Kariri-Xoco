8197366239c33823bf52c3b687b4ce9be3643f8a
edulpf0701 <edulpf0701@gmail.com>
2025-06-30 20:01:04 -0300

    Update tf_corpus.py

--- tf_corpus.py
+++ tf_corpus.py
@@ -15,8 +15,20 @@ Açaang bihé kayapri ayby tidzó Matyká potçõ sembohó buyõ uhá Sú yakawo
 Matyká babae ay erã ayby bihé ubumaná andé tidacrú sembohó sá bihé dzutsakó Bydiró ayby croté dzunuá buyõ kanghy doyghichy ay hohodé kayapri Perety potçõ cry pidéa warandzy andé sú buyõ kãwká ynatekié yee anhy ay dé raddá andé ay dé retsé Amé sodeyô ayby warandzy sú sayncró bihé sutú ayby hesã andé ydadé crikié mó aedzé tsohó sayncró dehé Ay Bidzamú tody buyõ kãwká sembohó ay netçoá ayby Matyká andé ká wohoyé ayby natiá mó woroby dó sú peretoá bihé barae yahí Matyká nó bité bihé Bidzamú andé wohoyé ayby natiá todyá buyõ kãwká amé sú andé amé kanghy barae yahé \
 Doró aedzé tó   poá mó  sú méa yetçã tçohó sembonó ayby ey nenhetá ayby kerú dó nó amepré sodeyô ay wché  nory ery tekerae  nó kié nó amepré sembohó sambyyê dó bihé kayapri ey ditery nenhetá buyõ nhanhikié ay dasá ayby hiaydé ayby barae andé kié ditery Açaang buréa sú mé  yetçã nory dó bihé kayapri yetçã bocuwyá nhanhikié wakié eridzá dasá  Bydiró ayby mará vró  aedzé tó tody açaang dodicekié crikiè açaang nusí mó ay tô hiaydé nhirõ aedzé tekerae dó kié netçoá ay dó mé \
 Kerú Tçãn kerú Tçãn andé andery ay dzé ayby bihé v-nú buyõ buré dó kié tçohó netçoá tokenhéa myghy amé nerú renghé ayby aedzé tsohó  Kerú Tçãn potçõ sembohó buyõ yré  buréa sembohó ay dasá ayby tó hiaydé andé bãrãbãrã ay mé-buré nenhetá dó ay hiaydé kié tçohóybá nerú  Aedzé tó bihé yee netço  nusí aedzé tekerae sitó tidacrú beté codoró sembohó hohoá méa ayby buré mó sembohó ay tó hiaydé  nerú kerú Tçãn wycri buyõ buré  kié dzené mekié ywakukié tçohó-ytú dó pidé sembohó ay sambyyê tarde \
-Bihé kayapri  kerú Tçãn wycri ká mó tidacrú bihé tobá ayby aedzé buhuú açaang bihé baté buyõ kunhy  bomodé ay hiaydé kié baewy  Buyõ ytuituté  tá kananekié aedzé buhuú andé sasá andé wycri eriwy kietçó ywakukié nó saerae nó doyghichy ayby yetcamyá beté anrá baté bomodé peretóa ay tobá  kerú Tçãn todyá sembohó kunhy andé bãrãbãrã ay mé-buré  dzudé  Dó kunhy  Vdjé eridzá tsohó kenhé enewy açaang bihé baté doró kunhy  Badzé sebéyamisã  Ybôá nerú ay dasá Yakawotçã dó sú tçohó kuatiará bihé potçó ayby wachany kayakuá andé  doró  kié wybae dzuy codoró ayby potçó docry \
-Andé dicry kayapri dó nó tçohoclyá wycri bihé wodicó  wobohó sú  bowró ayby mé-buré ayby hiaydé  kié pideá kenheá sembohó ay kunhy doró  wohoyé ay kayapri sú crikié mó ay hiaydé sacry hohó baté  Sodeyô nerú sú crikié mó ay hiaydé sacry nerú kunhy todyá Kerú Tçãn cry kié netçoá nerú ay dó tidacrú andé bãrãbãrã ay tody sembohó buyõ Banarekú crikié nhirõ mó cribuné dó wycri siniocribae buyõ-nerú  mó ay hiaydé dó sú tçohó mé-buré'
+Bihé kayapri  kerú Tçãn wycri ká mó tidacrú bihé tobá ayby aedzé buhuú açaang bihé baté buyõ kunhy  bomodé ay hiaydé kié baewy  Buyõ ytuituté  tá kananekié aedzé buhuú andé sasá andé wycri eriwy kietçó ywakukié nó saerae nó doyghichy ayby yetcamyá beté anrá baté bomodé peretóa ay tobá  kerú Tçãn todyá sembohó kunhy andé bãrãbãrã ay mé-buré  dzudé  Dó kunhy  Vdjé eridzá tsohó kenhé eniwy açaang bihé baté doró kunhy  Badzé sebéyamisã  Ybôá nerú ay dasá Yakawotçã dó sú tçohó kuatiará bihé potçó ayby wachany kayakuá andé  doró  kié wybae dzuy codoró ayby potçó docry \
+Andé dicry kayapri dó nó tçohoclyá wycri bihé wodicó  wobohó sú  bowró ayby mé-buré ayby hiaydé  kié pideá kenheá sembohó ay kunhy doró  wohoyé ay kayapri sú crikié mó ay hiaydé sacry hohó baté  Sodeyô nerú sú crikié mó ay hiaydé sacry nerú kunhy todyá Kerú Tçãn cry kié netçoá nerú ay dó tidacrú andé bãrãbãrã ay tody sembohó buyõ Banarekú crikié nhirõ mó cribuné dó wycri siniocribae buyõ-nerú  mó ay hiaydé dó sú tçohó mé-buré Wby dó ay hiaydé kié sacry andé kietçó nó kenhé sembohó ay kunhy  sú bãrãbãrã ay tody syupú  sembohó uhá andé buyõ uhaá  aedzé dasá radamü buyõ andé sú kié tçohó vdjé mé sembohó aedzé yetcamyá  wobohó amé myghy ayby andé wakié ayby ytú sembohó ay krodyá ayby retsé sú kié swbatkié wonhé ayby yeendé Kié nó méa sembohó ay dé raddá andé  buyõ tirerê  sembohó ay dé retsé \
+Bydiró ayby tçohoclyá kayapri buyõ syupú  kerú Tçãn tçohó bihé nhinkadeá ané açaang dó sú wby tidzó anrá raddá andé  bydiró ayby dzó  teró ay hiaydé andé  bydiró ayby hiaydé  bãrãbãrã ay sacry bihé nhinkadeá andé ayndy ubumaná ká velandinho ypoá mó ay hiaydé andé mó ay ubumaná tidacrú bihé dzutsakó  Perety potçõ  wycri mó retsé  crikié nhirõ ayby barae andé crikié sembohó nió mó sitó ay ubumaná dó tçohó wby anrá ané  Bydiró ayby wowoá buyõ  ydjé hohó ubumaná dicrodycelé  tidacrú ay dzutsakó andé croté Sú todyá warandzy andé açaang wy  ay hiaydé beté ay dzodzó  nhinkadeá andé Krody \
+Eridzá kayapri açaang wy  kerú Tçãn pereté nerú mé-buré ywakukié bihé ybuó ayby retsé wobohó sú Swbatkié dó bihé krodyá ay hohodé andé ekudú krodyá bihé tsohoá  Bá ay Krody ayby ytú andé ayby dzené netçoá andé swbatkié tokenhéa  Bá dó amepré andé swbatkié sembohó ay amepré ayby \
+pidé dzunú uká Tçate Bû Croté Tidacrú Benhekié Pepé Waikeitçu wby Barãbarã Naté Ynhahó ká Netó dzunú dzunú Ponhé Potçõ Ynhanaté dabá dadawy Dyoá Myghi eykó Aiuca Enké unú Nhirõ Ané Nhedé Wibae Bihéte Ydzeyá Tihiwy Eriwy Aêreçé Méechy \
+Wachány Wachanidikié Sumarã-orobae Mybihé-misã  Myrepri-bubihé Myrepri-Wachány Macribae-bihé tçohó baewy tá kenhé teudiokié nhaehy kieretú Tidzy Nhiké dzõ biké popó paidenhé dedenhé dé padzú dzutsoyê Karaytçy nhanhikié hohó enké nió puipú randé teró sypy arãkié dzé v-toná kutoá Krikié Krikié Ibará Prowy sambá sadó parú pepé peré Kãwká netçóa \
+Popó andé Byrae wycri popóa buyõ Piwonhé  Baté ayby tidacrú woryoentá açaang wohoyé andé wohoyédé dó Tçohó ay peréa ayby swbatkié  Popó netçóa buyõ ayby sitó  cry Byrae netçóa Nerú ayby sayncró  nerú v-djé wycri buyõ Piwonhé  bihé  Tidacrú yrandy hohodé  Perety Popó sitó  Byrae ekudú  perety Byrae sayncró  Popó pideá morohó andé mory súa wy  ydadé ekudú andé ytuituté Bihé kayapri  Popó ká Byrae mó sitó ande  Eridzá kayá ay sitóa pideá buyõ buré doyghichy dó Byrae  Wby dó aedzé Popó pideá ydezeyá amé kié tá Sitó ywakukié peré amé Bihé andé Byrae wy amé hohodé andé wycri wowoá Anrá retsé  todyá wycri nerú damã bihé ayby hohodé Doyghichy dó Popó nó myghy dó wybae pidé buyõ Damã ayby Byrae andé bãrãbãrã ay ká amé aedzé Popó  Byrae kié méa erá wy wybae wy retsé raddá'
+
+
+
+
+
+
+
 
 
 
@@ -25,7 +37,7 @@ cultura e nosso povo Antigamente os indígenas não queriam falar na língua por
 do homem branco Mas ela foi ensinada longe deles no Ouricuri sagrado Hoje ela  \
 sobrevive Vamos ter amor e falar nossa língua Sejam bem vindos \
 Eu sou professora da nossa língua Vamos estudar \
-você Beijo Tapioca/beiju Peixe Cidade/aldeia_de_pedra Espírito Anjo Linda Fazer Macaco Mata Poderoso/sagrado Terra Pequeno \
+você Beijo Tapioca/beiju Peixe Cidade/aldeia_de_pedra Espírito Anjo Linda Fazer Macaco Mata sagrado Terra Pequeno \
 cabeça olho nariz braço barriga perna pé mão orelha coração língua dente cabelo \
 galinha onça pássaro gato porco cachorro ovelha cavalo boi cobra macaco peixe \
 cantar comer cuscuz bom dia o do mãe terra verdadeiro ancestral ser muito sagrado estou com \
@@ -36,10 +48,20 @@ Matyká pegou as folhas de uma planta e fez com elas um chá Depois de tomar dor
 Então seu avô olhando para ele disse eu tenho dó de você  desejo de coração que se arrependa enquanto há tempo  porque meu neto  se não se arrepender com certeza que um dia você irá desejar muito sentir o calor do Sol de novo e não vai   Em rebeldia ele disse  eu duvido que um dia eu irei sentir falta desse calorão Depois de ouvir isso  seu avô ficou em silêncio pedindo em pensamento para o avô Sol perdoar seu neto  que não sabia o que dizia \
 Kerú Tçãn coração duro e é o nome de um menino muito rebelde  que não tinha saberes ancestrais contado pelos mais velhos do seu povo  Kerú Tçãn acordou com muito mau-humor  estressado com o calor do avô Sol e começou a xingar desejando que o sol não existisse mais  Seu avô  um grande sábio  repreendeu seu neto tentando fazer voltar atrás com aquelas palavras de ofensa para com o avô Sol  mas Kerú Tçãn era muito rebelde  não obedecia nada  nem  acreditando que estava com a verdade kieretú \
 Um dia  Kerú Tçãn foi chamado para fazer uma exposição de suas artes em um local muito frio  onde o Sol não aparecia  Muito entusiasmado  arrumou logo suas artes e roupas e foi viajar sem nem se quer se despedir da família chegar no local onde seria a exposição  Kerú Tçãn ficou com frio e começou a reclamar  nossa  Que frio  Como esse povo consegue viver em um lugar tão frio  Deus abençoe Sou mais o calor Só que ele tinha assinado um contrato de dois meses e  então  não poderia voltar antes do contrato terminar \
-E cada dia que se passava era uma tortura  pois ele  apesar de reclamar do Sol  não estava acostumado com o frio  então  todos os dias ele implorava para o Sol nascer naquele lugar  Quanto mais ele pedia para o Sol nascer mais frio ficava  Kerú Tçãn já não sabia mais o que fazer e começou a ficar com muito medo pedindo perdão para tudo que era criação principalmente  para o Sol que ele havia xingado'
+E cada dia que se passava era uma tortura  pois ele  apesar de reclamar do Sol  não estava acostumado com o frio  então  todos os dias ele implorava para o Sol nascer naquele lugar  Quanto mais ele pedia para o Sol nascer mais frio ficava  Kerú Tçãn já não sabia mais o que fazer e começou a ficar com muito medo pedindo perdão para tudo que era criação principalmente  para o Sol que ele havia xingado Vendo que o Sol não nascer e sem se acostumar com o frio  ele começou a ficar doente  com gripe e muita febre  sua imunidade baixou muito e ele não tinha como falar com seus familiares  pois por conta do e falta de fé com as forças da natureza ele não aprendeu cantos dos pássaros  Não se comunicava com a Mãe Terra e  muito menos  com a Mãe Natureza \
+Depois de passar dias muito doente  Kerú Tçãn teve um lindo sonho em que ele via chover na terra e  depois da chuva  vinha o Sol e  depois do Sol  começou a brotar uma linda e cheirosa planta chamada Velandinho olhar para o Sol e para a planta fazer um chá  Quando acordou  foi para mata  pediu perdão de novo e pediu com licença para procurar a planta que havia visto no sonho  Depois de caminhar bastante  encontrou aquela planta sagrada  fez o chá e tomou  Ele ficou curado e  em seguida  o Sol voltou a brilhar  lindo e forte \
+Desse dia em seguida  Kerú Tçãn nunca mais xingou nem um fenômeno da natureza  pois ele aprendeu que um fortalece o outro e juntos fortalece uma nação  Viva a força da fé e do respeito saberes e conhecimentos ancestrais  Viva que arrependem e aprendem com os erros de \
+estar dormir amar partir correr beber agir brincar jogar receber assistir começar trabalhar levar chamar  lembrar durmar dormer namorar   acordar  morrer  descansar continuar entrar  contar   possuir  mandar  chorar  doer   perdoar sonhar enviar poder apresentar sofrer  levantar visitar  respeitar   gritar \
+Segunda-feira Terça-feira Quarta-feira Quinta-feira Sexta-feira Sabado Domingo haver aparecer arrumar conseguir lutar resgatar tarde mulher avó sobrinho irmã irmão ito tia mãe pai café amanhã saudade aquele chorar sim fumar amigo vir luz céu nome pão livrar pedir peçar canoa cair tartaruga feio flor jogar sair feliz gostar \
+Popó e Byrae eram irmãos muito unidos  ponto de causar admiração em todos e todas que tinham a oportunidade de conhecer  Popó gostava muito de caçar  já Byrae gostava mais de plantar  mas como eram muito unidos  um fazia companhia outro  Quando Popó caçar  Byrae junto  quando Byrae plantar  Popó estava lá e  assim  eles seguiam  sempre juntos e contentes  Um dia  Popó chamou Byrae para caçar e nessa noite a caçada estava muito ruim até que Byrae  vendo que seu irmão estava triste por não conseguir caçar nenhum saiu por um e Byrae seguiu por outro e foram caminhando na mata  ficando cada mais distantes um do outro até que Popó se conta que poderia estar muito longe de Byrae e começou a chamar pelo seu irmão  Byrae não respondia casa seguir poder andar mata chão'
 
 
-animais = ['sabuká', 'homomocleclé', 'yeendé', 'panhõ', 'kurê', 'bukuté', 'ceprum', 'cabarú', 'cradzó', 'nhienhy', 'dzikuá', 'midzé']
+
+
+
+
+
+animais = ['sabuká', 'homomocleclé', 'yeendé', 'panhõ', 'kurê', 'bukuté', 'ceprum', 'cabarú', 'cradzó', 'nhienhy', 'dzikuá', 'midzé', 'sambá']
 	
 resposta_certa_animais = {
 	'sabuká': 'galinha',
@@ -53,7 +75,8 @@ resposta_certa_animais = {
 	'cradzó': 'boi', 
 	'nhienhy': 'cobra', 
 	'dzikuá': 'macaco', 
-	'midzé': 'peixe'}
+	'midzé': 'peixe',
+	'sambá': 'tartaruga'}
 	
 	
 pch = ['tçambú', 'pó', 'namby', 'bó', 'abyró', 'wô', 'puih', 'mysã', 'benhié', 'kerú', 'nunú', 'dzá', 'suy']
@@ -93,12 +116,3 @@ resposta_certa_frases = {
 	'Ey andery bihé marã': 'você é uma guerreira'}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-
